0e2e0d4f0c00dfbe4d3ef999d90bf95b3ce72c18
Mukherjee, Debangshu <mukherjeed@ornl.gov>
2019-06-17 19:33:14 -0400

    Change number of function evals permitted

--- stemtools/eels/eels_tools.py
+++ stemtools/eels/eels_tools.py
@@ -35,7 +35,7 @@ def func_powerlaw(x, m, c):
 def powerlaw_fit(xdata,ydata,xrange):
     start_val = np.int((xrange[0] - np.amin(xdata))/(np.median(np.diff(xdata))))
     stop_val = np.int((xrange[1] - np.amin(xdata))/(np.median(np.diff(xdata))))
-    popt, _ = spo.curve_fit(func_powerlaw, xdata[start_val:stop_val], ydata[start_val:stop_val])
+    popt, _ = spo.curve_fit(func_powerlaw, xdata[start_val:stop_val], ydata[start_val:stop_val],maxfev=10000)
     fitted = func_powerlaw(xdata,popt[0],popt[1])
     return fitted, popt
 
@@ -64,7 +64,9 @@ def region_intensity(xdata,ydata,xrange,peak_range,showdata=True):
     subtracted_data = ydata - fitted_data
     start_val = np.int((peak_range[0] - np.amin(xdata))/(np.median(np.diff(xdata))))
     stop_val = np.int((peak_range[1] - np.amin(xdata))/(np.median(np.diff(xdata))))
-    peak_ratio = np.sum(subtracted_data[start_val:stop_val])/np.sum(fitted_data)
+    data_floor = np.amin(subtracted_data[start_val:stop_val])
+    peak_ratio = np.sum(subtracted_data[start_val:stop_val] - data_floor)/np.sum(fitted_data - data_floor)
+    peak_sum = np.sum(subtracted_data[start_val:stop_val] - data_floor)
     yrange = np.zeros_like(peak_range)
     yrange[0] = subtracted_data[start_val]
     yrange[1] = subtracted_data[stop_val]
@@ -78,7 +80,7 @@ def region_intensity(xdata,ydata,xrange,peak_range,showdata=True):
         plt.ylabel('Intensity (A.U.)')
         plt.title('Sum from region = {}'.format(peak_ratio))
         plt.xlim(np.amin(xdata),np.amax(xdata))
-    return peak_ratio
+    return peak_ratio, peak_sum
 
 @numba.jit(parallel=True)
 def eels_3D(eels_dict,fit_range,peak_range,clean_val=0):
@@ -88,9 +90,11 @@ def eels_3D(eels_dict,fit_range,peak_range,clean_val=0):
     else:
         eels_clean = eels_array
     xdata = (np.arange(eels_clean.shape[0]) - eels_dict['pixelOrigin'][0])*eels_dict['pixelSize'][0]
-    peak_val = np.zeros((eels_clean.shape[-2],eels_clean.shape[-1]), dtype=np.float64)
+    rel_peak_val = np.zeros((eels_clean.shape[-2],eels_clean.shape[-1]), dtype=np.float64)
+    abs_peak_val = np.zeros((eels_clean.shape[-2],eels_clean.shape[-1]), dtype=np.float64)
     for jj in numba.prange(eels_clean.shape[-1]):
         for ii in range(eels_clean.shape[-2]):
-            peak_val[ii,jj] = region_intensity(xdata,eels_clean[:,ii,jj],fit_range,peak_range,showdata=False)
-    peak_val = peak_val - np.amin(peak_val)
-    return peak_val
+            pv,ps = region_intensity(xdata,eels_clean[:,ii,jj],fit_range,peak_range,showdata=False)
+            rel_peak_val[ii,jj] = pv
+            abs_peak_val[ii,jj] = ps
+    return rel_peak_val, abs_peak_val
