d6006ccecc8b47d935db09ddb66373a0b386105b
Mukherjee, Debangshu <mukherjeed@ornl.gov>
2019-06-20 17:19:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-with open(os.path.join(here, 'stemtools/__version__.py')) as f:
-    __version__ = f.read().split("'")[1]
+exec(open('stemtools/__version__.py').read())
 
 setup(name='stemtools',
     version=__version__,
@@ -11,9 +10,10 @@ setup(name='stemtools',
     url='https://code.ornl.gov/7dm/stemtools',
     author='Debangshu Mukherjee',
     author_email='mukherjeed@ornl.gov',
-    license='GNU GPLv3',
+    license='MIT',
     keywords="STEM",
     install_requires=[
         'numpy >= 1.15, < 2.0',
         'scipy >= 1.1, < 1.2',
+        'numba >= 0.13.3, < 0.41.0',
     ])
