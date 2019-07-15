6ea9500bb338fbec215fd33de959cff64e278381
Debangshu Mukherjee <mukherjeed@ornl.gov>
2019-07-15 15:28:46 -0400

    Better NBED functions using log of sobel filters, faster aperture images using numpy arrays and more complete documentation

--- stemtools/nbed/nbed_strain.py
+++ stemtools/nbed/nbed_strain.py
@@ -1,17 +1,60 @@
 import numpy as np
 from scipy import ndimage as scnd
 from scipy import optimize as sio
+from scipy import signal as scisig
+import matplotlib.pyplot as plt
 from ..util import image_utils as iu
 from ..proc import sobel_canny as sc
 from ..util import gauss_utils as gt
 
-def angle_fun(angle,image_orig):
+def angle_fun(angle,image_orig,axis=0,):
+    """
+    Rotation Sum Finder
+    
+    Parameters
+    ----------
+    angle:      float 
+                Angle to rotate 
+    image_orig: (2,2) shape ndarray
+                Input Image
+    axis:       int
+                Axis along which to perform sum
+                     
+    Returns
+    -------
+    rotmin: float
+            Sum of the rotated image multiplied by -1 along 
+            the axis specified
+                 
+    :Authors:
+    Debangshu Mukherjee <mukherjeed@ornl.gov>
+    """
     rotated_image = scnd.rotate(image_orig,angle,order=5,reshape=False)
     rotsum = (-1)*(np.sum(rotated_image,1))
     rotmin = np.amin(rotsum)
     return rotmin
 
-def rotation_finder(image_orig):
+def rotation_finder(image_orig,axis=0):
+    """
+    Angle Finder
+    
+    Parameters
+    ----------
+    image_orig: (2,2) shape ndarray
+                Input Image
+    axis:       int
+                Axis along which to perform sum
+                     
+    Returns
+    -------
+    min_x: float
+           Angle by which if the image is rotated
+           by, the sum of the image along the axis
+           specified is maximum
+                 
+    :Authors:
+    Debangshu Mukherjee <mukherjeed@ornl.gov>
+    """
     x0 = 90
     x = sio.minimize(angle_fun,x0,args=(image_orig))
     min_x = x.x
@@ -59,210 +102,101 @@ def rotate_and_center_ROI(data4D_ROI,rotangle,xcenter,ycenter):
         corrected_ROI[ii,:,:] = rotated_cbed
     return corrected_ROI
 
-def correlate_4D(data4D,corr_pattern,hybridizer=0.5):
+def data4Dto2D(data4D):
     """
-    Hybrid cross-correlate a 4D-STEM dataset's individual patterns
-    with a known correlation pattern.
+    Convert 4D data to 2D data
     
     Parameters
     ----------
-    data4D: ndarray
-            4D-STEM dataset to be correlated where
-            the first two dimensions are in Fourier
-            space and the last two dimensions are 
-            in real space.
-    corr_pattern: ndarray
-                  Two-dimensional array of the pattern
-                  that is to be correlated with each 
-                  individual CBED pattern.
-    hybridizer: float
-                Hybrid Cross Correlation parameter
-                Default is 0.5
-                
+    data4D: ndarray of shape (4,4)
+            the first two dimensions are Fourier
+            space, while the next two dimensions
+            are real space
+                     
     Returns
     -------
-    corr4D: ndarray
-            Complex valued 4D dataset where the 
-            first two dimensions are the correlated
-            patterns.
-            
+    data2D: ndarray of shape (2,2)
+            Raveled 2D data where the
+            first two dimensions are positions
+            while the next two dimensions are spectra
+                 
     :Authors:
     Debangshu Mukherjee <mukherjeed@ornl.gov>
     """
-    data_size = (np.asarray(data4D.shape)).astype(int)
-    corr4D = (np.zeros((data_size[0],data_size[1],data_size[2],data_size[3]))).astype('complex128')
-    for jj in range(data_size[3]):
-        for ii in range(data_size[2]):
-            cbed = data4D[:,:,ii,jj]
-            cc_cbed = iu.normalized_correlation(cbed,corr_pattern,hybridizer)
-            corr4D[:,:,ii,jj] = cc_cbed
-    return corr4D
+    data2D = np.transpose(data4D,(2,3,0,1))
+    data_shape = data2D.shape
+    data2D.shape = (data_shape[0]*data_shape[1],data_shape[2]*data_shape[3])
+    return data2D
 
-def correlate_with_disk(data4D,radius,hybridizer=0.5):
+def bin4D(data4D,bin_factor):
     """
-    Hybrid cross-correlate a 4D-STEM dataset's 
-    individual patterns with a circle whose radius 
-    is user defined.
+    Bin 4D data in spectral dimensions
     
     Parameters
     ----------
-    data4D: ndarray
-            4D-STEM dataset to be correlated where
-            the first two dimensions are in Fourier
-            space and the last two dimensions are 
-            in real space.
-    radius: float
-            Radius of the circle for correlation
-    hybridizer: float
-                Hybrid Cross Correlation parameter
-                Default is 0.5
-                
+    data4D:     ndarray of shape (4,4)
+                the first two dimensions are Fourier
+                space, while the next two dimensions
+                are real space
+    bin_factor: int
+                Value by which to bin data
+                     
     Returns
     -------
-    corr4D: ndarray
-            Complex valued 4D dataset where the 
-            first two dimensions are the correlated
-            patterns.
+    binned_data: ndarray of shape (4,4)
+                 Data binned in the spectral dimensions
     
-    See Also
-    --------
-    correlate4D
-    make_circle
-            
+    Notes
+    -----
+    The data is binned in the last two spectral dimensions
+    using scipy signal decimate
+                 
     :Authors:
     Debangshu Mukherjee <mukherjeed@ornl.gov>
     """
-    data_size = (np.asarray(data4D.shape)).astype(int)
-    central_disk = iu.make_circle(data_size,(data_size[1] / 2), (data_size[0] / 2), radius)
-    corr4D = (np.zeros((data_size[0],data_size[1],data_size[2],data_size[3]))).astype('complex128')
-    for jj in range(data_size[3]):
-        for ii in range(data_size[2]):
-            cbed = data4D[:,:,ii,jj]
-            cc_cbed = iu.normalized_correlation(cbed,central_disk,hybridizer)
-            corr4D[:,:,ii,jj] = cc_cbed
-    return corr4D
-
-def gaussian_fit_corr(corr_data,radius_circ,center_distance):
-    corr_centers = np.zeros((3,3,2))
-    data_size = (np.asarray(corr_data.shape)).astype(int)
-    yV, xV = np.mgrid[0:data_size[0],0:data_size[1]]
-    for jj in range(3):
-        for ii in range(3):
-            pp = ii - 1
-            qq = jj - 1
-            center_x = (data_size[1] / 2) + (pp*center_distance)
-            center_y = (data_size[0] / 2) + (qq*center_distance)
-            first_circ = (iu.make_circle(data_size,center_x,center_y,radius_circ)).astype(bool)
-            xx = xV[first_circ]
-            yy = yV[first_circ]
-            zz = corr_data[first_circ]
-            center_x = xx[zz == np.amax(zz)]
-            center_y = yy[zz == np.amax(zz)]
-            gauss_ii_jj = gt.fit_gaussian2D_mask(corr_data,center_x,center_y,radius_circ,'circular')
-            corr_centers[ii,jj,0] = gauss_ii_jj[0]
-            corr_centers[ii,jj,1] = gauss_ii_jj[1]
-    return corr_centers
-
-def get_ROI(image,top_left,top_right,bottom_left,bottom_right):
-    p,q = np.shape(image)
-    xV, yV = np.meshgrid(np.arange(p),np.arange(q))
-    top_slope = (top_right[1] - top_left[1])/(top_right[0] - top_left[0])
-    top_intercept = top_right[1] - (top_slope*top_right[0])
-    sub_top = (yV > (top_intercept + (top_slope*xV)))
-    left_slope = (bottom_left[1] - top_left[1])/(bottom_left[0] - top_left[0])
-    left_intercept = top_left[1] - (left_slope*top_left[0])
-    sub_left = (yV < (left_intercept + (left_slope*xV)))
-    right_slope = (bottom_right[1] - top_right[1])/(bottom_right[0] - top_right[0])
-    right_intercept = top_right[1] - (right_slope*top_right[0])
-    sub_right = (yV > (right_intercept + (right_slope*xV)))
-    bottom_slope = (bottom_right[1] - bottom_left[1])/(bottom_right[0] - bottom_left[0])
-    bottom_intercept = bottom_right[1] - (bottom_slope*bottom_right[0])
-    sub_bottom = (yV < (bottom_intercept + (bottom_slope*xV)))
-    sub = np.multiply(np.multiply(sub_top,sub_bottom),np.multiply(sub_left,sub_right))
-    xR = np.ravel(xV[sub])
-    yR = np.ravel(yV[sub])
-    return sub, xR, yR
-
-def correlate_4D_ROI_test(data4D_ROI,corr_pattern,hybridizer=0.5):
-    corr4D_raw = np.zeros_like(data4D_ROI,dtype=np.double) #raw data
-    corr4D_log = np.zeros_like(data4D_ROI,dtype=np.double) #log of the data
-    corr4D_sob = np.zeros_like(data4D_ROI,dtype=np.double) #with the sobel filtered data
-    corr4D_lsb = np.zeros_like(data4D_ROI,dtype=np.double) #with the log of the sobel filtered data
-    
-    corr_pattern_raw = corr_pattern
-    corr_pattern_log = np.log10(1 + corr_pattern_raw - np.amin(corr_pattern_raw))
-    corr_pattern_sob, _ = sc.sobel_filter(corr_pattern_raw)
-    corr_pattern_lsb = np.log10(1 + corr_pattern_sob - np.amin(corr_pattern_sob))
-    for ii in numba.prange(data4D_ROI.shape[0]):
-        cbed_raw = data4D_ROI[ii,:,:]
-        cbed_log = np.log10(1 + cbed_raw - np.amin(cbed_raw))
-        cbed_sob, _ = sc.sobel_filter(cbed_raw)
-        cbed_lsb = np.log10(1 + cbed_sob - np.amin(cbed_sob))
-        
-        cc_cbed_raw = np.abs(iu.normalized_correlation(cbed_raw,corr_pattern_raw,hybridizer))
-        cc_cbed_log = np.abs(iu.normalized_correlation(cbed_log,corr_pattern_log,hybridizer))
-        cc_cbed_sob = np.abs(iu.normalized_correlation(cbed_sob,corr_pattern_sob,hybridizer))
-        cc_cbed_lsb = np.abs(iu.normalized_correlation(cbed_lsb,corr_pattern_lsb,hybridizer))
-        
-        corr4D_raw[ii,:,:] = cc_cbed_raw
-        corr4D_log[ii,:,:] = cc_cbed_log
-        corr4D_sob[ii,:,:] = cc_cbed_sob
-        corr4D_lsb[ii,:,:] = cc_cbed_lsb
-    return corr4D_raw, corr4D_log, corr4D_sob, corr4D_lsb
-
-def gaussian_fit_4D_ROI(corr_data_ROI,radius_circ,center_distance):
-    data_size = np.asarray(np.shape(corr_data_ROI))
-    fitted_diff_points = np.zeros((data_size[0],3,3,2))
-    for ii in range(data_size[0]):
-        corr_cbed = np.abs(corr_data_ROI[ii,:,:])
-        corr_points = gaussian_fit_corr(corr_cbed,radius_circ,center_distance)
-        fitted_diff_points[ii,:,:,:] = corr_points
-    return fitted_diff_points
-
-def strain_in_ROI(fitted_cbed,p_matrix,mean_centers,points):
-    no_of_disks = int(0.5*(np.size(mean_centers)))
-    b_mean = np.reshape(mean_centers, (no_of_disks,2))
-    b_mean = b_mean - b_mean[4,:]
-    data_size = (np.shape(fitted_cbed))[0]
-    e_xx_ROI = np.zeros(data_size)
-    e_yy_ROI = np.zeros(data_size)
-    e_xy_ROI = np.zeros(data_size)
-    e_th_ROI = np.zeros(data_size)
-    identity_matrix =  np.asarray(((1,0),
-                                   (0,1)))
-    l_mean, _,_,_ = np.linalg.lstsq(p_matrix[points,:],b_mean[points,:],rcond=None)
-    l_mean_inv = np.linalg.inv(l_mean)
-    
-    for ii in range(data_size):
-        corr_points = fitted_cbed[ii,:,:,:]
-        b_cbed = np.reshape(corr_points, (no_of_disks,2))
-        b_cbed = b_cbed - b_cbed[4,:]
-        l_cbed, _,_,_ = np.linalg.lstsq(p_matrix[points,:],b_cbed[points,:],rcond=None)
-        t_cbed = np.matmul(l_cbed,l_mean_inv)
-        s_cbed = t_cbed - identity_matrix
-        e_xx = -s_cbed[0,0]
-        e_yy = -s_cbed[1,1]
-        e_xy = -(s_cbed[0,1] + s_cbed[1,0])
-        e_th = (s_cbed[0,1] - s_cbed[1,0])
-        e_xx_ROI[ii] = e_xx
-        e_yy_ROI[ii] = e_yy
-        e_xy_ROI[ii] = e_xy
-        e_th_ROI[ii] = e_th
-    
-    return e_xx_ROI, e_xy_ROI, e_yy_ROI, e_th_ROI
-
-def ROI_to_map(strain_ROI,image,ROI_range,med_value=0.25):
-    y_ROI = ROI_range[0]
-    x_ROI = ROI_range[1]
-    z_ROI = iu.image_normalizer(image[y_ROI,x_ROI])
-    ROI_box = np.zeros_like(image)
-    ROI_box[y_ROI[z_ROI > med_value*np.median(z_ROI)],x_ROI[z_ROI > med_value*np.median(z_ROI)]] = 1
-    strain_map = np.zeros_like(image)
-    strain_map[y_ROI,x_ROI] = strain_ROI
-    strain_map = np.multiply(strain_map,ROI_box)
-    return strain_map
+    mean_data = np.mean(np.mean(data4D,-1),-1)
+    mean_binned = scisig.decimate(scisig.decimate(mean_data,bin_factor,axis=0),bin_factor,axis=1)
+    binned_data = np.zeros((mean_binned.shape[0],mean_binned.shape[1],data4D.shape[2],data4D.shape[3]),dtype=np.float)
+    for ii in range(data4D.shape[2]):
+        for jj in range(data4D.shape[3]):
+            ronchi = data4D[:,:,ii,jj]
+            binned_ronchi = scisig.decimate(scisig.decimate(ronchi,bin_factor,axis=0),bin_factor,axis=1)
+            binned_data[:,:,ii,jj] = binned_ronchi
+    return binned_data
 
 def test_aperture(pattern,center,radius,showfig=True):
+    """
+    Test an aperture position for Virtual DF image
+    
+    Parameters
+    ----------
+    pattern: ndarray of shape (2,2)
+             Diffraction pattern, preferably the
+             mean diffraction pattern for testing out
+             the aperture location
+    center:  ndarray of shape (1,2)
+             Center of the circular aperture
+    radius:  float
+             Radius of the circular aperture
+    showfig: bool
+             If showfig is True, then the image is
+             displayed with the aperture overlaid
+                     
+    Returns
+    -------
+    aperture: ndarray of shape (2,2)
+              A matrix of the same size of the input image
+              with zeros everywhere and ones where the aperture
+              is supposed to be
+    
+    Notes
+    -----
+    Use the showfig option to visually test out the aperture 
+    location with varying parameters
+                 
+    :Authors:
+    Debangshu Mukherjee <mukherjeed@ornl.gov>
+    """
     center = np.asarray(center)
     yy,xx = np.mgrid[0:pattern.shape[0],0:pattern.shape[1]]
     yy = yy - center[1]
@@ -276,17 +210,49 @@ def test_aperture(pattern,center,radius,showfig=True):
     return aperture
 
 def aperture_image(data4D,center,radius):
-    center = np.asarray(center)
+    """
+    Generate Virtual DF image for a given aperture
+    
+    Parameters
+    ----------
+    data4D: ndarray of shape (4,4)
+            the first two dimensions are Fourier
+            space, while the next two dimensions
+            are real space
+    center: ndarray of shape (1,2)
+            Center of the circular aperture
+    radius: float
+            Radius of the circular aperture
+    
+    Returns
+    -------
+    df_image: ndarray of shape (2,2)
+              Generated virtual dark field image
+              from the aperture and 4D data
+    
+    Notes
+    -----
+    We generate the aperture first, and then make copies
+    of the aperture to generate a 4D dataset of the same 
+    size as the 4D data. Then we do an element wise 
+    multiplication of this aperture 4D data with the 4D data
+    and then sum it along the two Fourier directions.
+                 
+    :Authors:
+    Debangshu Mukherjee <mukherjeed@ornl.gov>
+    """
+    center = np.array(center)
     yy,xx = np.mgrid[0:data4D.shape[0],0:data4D.shape[1]]
     yy = yy - center[1]
     xx = xx - center[0]
     rr = ((yy ** 2) + (xx ** 2)) ** 0.5
-    aperture = np.asarray(rr<=radius, dtype=np.double)
-    image = np.zeros((data4D.shape[2],data4D.shape[3]),dtype=np.double)
-    for ii in range(data4D.shape[2]):
-        for jj in range(data4D.shape[3]):
-            image[ii,jj] = np.sum(np.multiply(aperture,data4D[:,:,ii,jj]))
-    return image
+    aperture = np.asarray(rr<=radius, dtype=data4D.dtype)
+    apt_copy = np.empty((data4D.shape[2],data4D.shape[3]) + aperture.shape,dtype=data4D.dtype)
+    apt_copy[:] = aperture
+    apt_copy = np.transpose(apt_copy,(2,3,0,1))
+    apt_mult = apt_copy * data4D
+    df_image = np.sum(np.sum(apt_mult,axis=0),axis=0)
+    return df_image
 
 def ROI_from_image(image,med_val,style='over',showfig=True):
     if style == 'over':
@@ -297,5 +263,53 @@ def ROI_from_image(image,med_val,style='over',showfig=True):
         plt.figure(figsize=(15, 15))
         plt.imshow(ROI+iu.image_normalizer(image),cmap='viridis')
         plt.title('ROI overlaid')
-        plt.axis('off')
+    ROI = ROI.astype(bool)
     return ROI
+
+def get_disk_fit(corr_image,disk_size,disk_list,pos_list):
+    fitted_disk_list = np.zeros(np.shape(disk_list),dtype=np.float)
+    disk_locations = np.zeros(np.shape(disk_list),dtype=np.float)
+    for ii in range(np.shape(disk_list)[0]):
+        posx = disk_list[ii,0]
+        posy = disk_list[ii,1]
+        par = gt.fit_gaussian2D_mask(corr_image,posx,posy,disk_size)
+        fitted_disk_list[ii,0] = par[0]
+        fitted_disk_list[ii,1] = par[1]
+    disk_locations[:,0] = (-1)*fitted_disk_list[:,0]
+    disk_locations[:,1] = fitted_disk_list[:,1]
+    center = disk_locations[np.logical_and((pos_list[:,0] == 0),(pos_list[:,1] == 0)),:]
+    disk_locations[:,0:2] = disk_locations[:,0:2] - center
+    lcbed,_,_,_ = np.linalg.lstsq(pos_list,disk_locations,rcond=None)
+    center[0,0] = (-1)*center[0,0]
+    return fitted_disk_list,center,lcbed
+
+def strain_in_ROI(data4D_ROI,center_disk,disk_list,pos_list,reference_axes):
+    # Calculate needed values
+    no_of_disks = data4D_ROI.shape[-1]
+    disk_size = (np.sum(center_disk)/np.pi) ** 0.5
+    i_matrix = (np.eye(2)).astype(np.float)
+    # Initialize matrices
+    e_xx_ROI = np.zeros(no_of_disks,dtype=np.float)
+    e_xy_ROI = np.zeros(no_of_disks,dtype=np.float)
+    e_yy_ROI = np.zeros(no_of_disks,dtype=np.float)
+    #Calculate for mean CBED if no reference
+    #axes present
+    if np.size(reference_axes) < 2:
+        mean_cbed = np.mean(data4D_ROI,axis=-1)
+        log_mean_cbed = st.util.image_logarizer(mean_cbed,16)
+        log_mean_corr = st.util.cross_corr(log_mean_cbed,center_disk,hybridizer=0.1)
+        _,_,mean_axes = get_disk_fit(log_mean_corr,disk_size,disk_list,pos_list)
+        inverse_axes = np.linalg.inv(mean_axes)
+    else:
+        inverse_axes = np.linalg.inv(reference_axes)
+    for ii in range(no_of_disks):
+        pattern = data4D_ROI[:,:,ii]
+        log_pattern = iu.image_logarizer(pattern,16)
+        corr_pattern = iu.cross_corr(log_pattern,center_disk,hybridizer=0.1)
+        _,_,pattern_axes = get_disk_fit(corr_pattern,disk_size,disk_list,pos_list)
+        t_pattern = np.matmul(pattern_axes,inverse_axes)
+        s_pattern = t_pattern - i_matrix
+        e_xx_ROI[ii] = -s_pattern[0,0]
+        e_xy_ROI[ii] = -(s_pattern[0,1] + s_pattern[1,0])
+        e_yy_ROI[ii] = -s_pattern[1,1]
+    return e_xx_ROI,e_xy_ROI,e_yy_ROI
