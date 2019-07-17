d47ea36facfffed2c4f3cbc5959e79a30cc8691b
Mukherjee, Debangshu <mukherjeed@ornl.gov>
2019-07-17 11:16:21 -0400

    Update setup.py to reflect dependencies

--- setup.py
+++ setup.py
@@ -13,8 +13,12 @@ setup(name='stemtools',
     license='MIT',
     keywords="STEM",
     install_requires=[
-        'numpy >= 1.13.0, < 1.16.4',
-        'scipy >= 1.0.0, < 1.3.0',
-        'numba >= 0.13.3, < 0.41.0',
-        'pyfftw >= 0.10.2, < 0.11.1',
+        'numpy >= 1.13.0',
+        'scipy >= 1.0.0',
+        'skimage >= 0.11.3'
+        'pywt >= 0.5.2'
+        'pillow > 5.0.0'
+        'matplotlib >= 2.2.0'
+        'numba >= 0.35.0',
+        'pyfftw >= 0.10.2',
     ])
