a6d0b2c28f6b3a047a6450bb4f2ab70d18788968
Mukherjee, Debangshu <mukherjeed@ornl.gov>
2020-05-04 11:43:30 -0400

    Modified steup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 from setuptools import setup, find_packages
 import os
 
-exec(open('stemtools/__version__.py').read())
+exec(open('stemtool/__version__.py').read())
+with open('README.md') as f:
+    long_description = f.read()
 
-setup(name='stemtools',
+setup(name='stemtool',
     version=__version__,
     packages=find_packages(),
-    description='Analysis of aberration corrected STEM data and 4D STEM data',
-    url='https://code.ornl.gov/7dm/stemtools',
+    description='A single package for analyzing atomic resolution STEM, 4D-STEM and STEM-EELS datasets, along with basic STEM simulation functionality',
+    url='https://github.com/stemtool/stemtool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Debangshu Mukherjee',
     author_email='mukherjeed@ornl.gov',
     license='MIT',
-    keywords="STEM",
+    keywords = ['STEM','EELS','4D-STEM','electron microscopy'],
+    zip_safe=False,
     install_requires=[
         'numpy >= 1.13.0',
         'scipy >= 1.0.0',
-        'skimage >= 0.11.3'
-        'pywt >= 0.5.2'
-        'pillow > 5.0.0'
-        'matplotlib >= 2.2.0'
+        'scikit-image >= 0.13.0',
+        'pywavelets >= 0.5.2',
+        'pillow > 5.0.0',
+        'matplotlib >= 2.2.0',
         'numba >= 0.45.0',
+        'pyfftw >= 0.10.3',
     ])
