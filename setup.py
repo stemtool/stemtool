34fa3b549f864efbd7e348b987ae876a03765670
Debangshu Mukherjee <mukherjeed@ornl.gov>
2020-05-20 18:56:48 -0400

    fixed indent issue

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(name='stemtool',
     # The C libraries required to build stemtool aren't available on RTD, 
     # so we need to exclude it from the installed dependencies here, and 
     # mock it for import in docs/conf.py using the autodoc_mock_imports parameter:
-    if not os.getenv('READTHEDOCS'):
-        install_requires.append('pyfftw >= 0.10.3', 
-                                'pillow > 5.0.0', 
-                                'imagecodecs >= 2019.1.1', 
-                                'numba >= 0.45.0', 
-                                'scikit-image >= 0.13.0',)
+if not os.getenv('READTHEDOCS'):
+    install_requires.append('pyfftw >= 0.10.3', 
+                            'pillow > 5.0.0', 
+                            'imagecodecs >= 2019.1.1', 
+                            'numba >= 0.45.0', 
+                            'scikit-image >= 0.13.0',)
