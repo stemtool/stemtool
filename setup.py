0c6f8fb7eccc2c35f661207b1e3b76f4f2f9cfb8
Debangshu Mukherjee <mukherjeed@ornl.gov>
2020-05-20 18:05:31 -0400

    Update setup.py
    Modified `setup.py` for readthedocs

--- setup.py
+++ setup.py
@@ -20,11 +20,15 @@ setup(name='stemtool',
     install_requires=[
         'numpy >= 1.13.0',
         'scipy >= 1.0.0',
-        'scikit-image >= 0.13.0',
         'pywavelets >= 0.5.2',
-        'pillow > 5.0.0',
         'matplotlib >= 2.2.0',
-        'numba >= 0.45.0',
-        'pyfftw >= 0.10.3',
-        'imagecodecs >= 2019.1.1',
     ])
+    # The C libraries required to build stemtool aren't available on RTD, 
+    # so we need to exclude it from the installed dependencies here, and 
+    # mock it for import in docs/conf.py using the autodoc_mock_imports parameter:
+    if not os.getenv('READTHEDOCS'):
+        install_requires.append('pyfftw >= 0.10.3', 
+                                'pillow > 5.0.0', 
+                                'imagecodecs >= 2019.1.1', 
+                                'numba >= 0.45.0', 
+                                'scikit-image >= 0.13.0',)
