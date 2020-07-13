0d6e0d0809aa5727b16459fc34a5c1142d63acb0
Debangshu Mukherjee <mukherjeed@ornl.gov>
2020-07-13 00:30:27 -0400

    Modified frms6 file loader to read in multiple files

--- stemtool/util/pnccd.py
+++ stemtool/util/pnccd.py
@@ -5,6 +5,8 @@ import numpy as np
 import h5py
 import stemtool as st
 import numba
+import glob
+import os
 from collections import OrderedDict
 
 class Frms6Reader(object):
@@ -427,13 +429,44 @@ def getDataSize(filename, path='/stream'):
 
     return f[path].shape
 
-def get_data_ref(data_string):
-    dref_string = data_string[0:-7] + '0' + data_string[-6:-1] + data_string[-1]
+def get_data_ref(data_dir):
+    os.chdir(data_dir)
     data_class = st.util.Frms6Reader()
-    data_shape = np.asarray(data_class.getDataShape(data_string),dtype=int)
-    dref_shape = np.asarray(data_class.getDataShape(dref_string),dtype=int)
-    dark_ref = data_class.readData(dref_string, image_range=(0,dref_shape[-1]), pixels_x = dref_shape[0], pixels_y = dref_shape[1])
-    data_3D = data_class.readData(data_string, image_range=(0,data_shape[-1]), pixels_x = data_shape[0], pixels_y = data_shape[1])
+    tot_files = 0
+    for file in glob.glob('*.frms6'):
+        tot_files += 1
+    filesizes = np.empty((tot_files, 4), dtype=int)
+
+    ii = 0
+    for file in glob.glob('*.frms6'):
+        fname = data_dir+file
+        dshape = np.asarray(data_class.getDataShape(fname), dtype=int)
+        filesizes[ii, 0:3] = dshape 
+        filesizes[ii, -1] = fname[-7]
+        ii += 1
+
+    dref_shape = filesizes[filesizes[:, -1]==0, 0:3][0]
+    data_shape = np.copy(dref_shape)
+    data_shape[-1] = (np.sum(filesizes[:, -2]) - np.amin(filesizes[:, -2])).astype(int)
+    data_3D = np.empty(data_shape)
+    draw_shape = (np.mean(filesizes[filesizes[:, -1]!=0, 0:3], axis=0)).astype(int)
+
+    ii = 0
+    for file in glob.glob('*.frms6'):
+        fname = data_dir+file
+        if (filesizes[ii, -1] == 0):
+            dark_ref = data_class.readData(fname, image_range=(0, dref_shape[-1]), 
+                                           pixels_x = dref_shape[0], 
+                                           pixels_y = dref_shape[1])
+        else:
+            frms6_num = filesizes[ii, -1]
+            start_fill = int((frms6_num - 1)*draw_shape[-1])
+            stop_fill = int(frms6_num*draw_shape[-1])
+            data_3D[:, :, start_fill:stop_fill] = data_class.readData(fname, 
+                                                                      image_range=(0, draw_shape[-1]), 
+                                                                      pixels_x = draw_shape[0], 
+                                                                      pixels_y = draw_shape[1])
+            ii += 1
     return data_3D, dark_ref
 
 @numba.jit(cache=True, parallel=True)
