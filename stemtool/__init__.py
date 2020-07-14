a03e1a4b4ae51cd1124f87e30c65678a502f206d
Debangshu Mukherjee <mukherjeed@ornl.gov>
2020-07-14 14:31:16 -0400

    Update __init__.py

--- stemtool/__init__.py
+++ stemtool/__init__.py
@@ -3,11 +3,10 @@ from .__version__ import __version__
 __all__ = ['__version__']
 
 from . import afit
-from . import beam
+from . import dpc
 from . import eels
 from . import gpa
 from . import nbed
-from . import util
-from . import dpc
 from . import sim
-from .code_timer import *
+from . import util
+from .timer import *
