c0de057a4ab883f0489de4c247f6c36dc6387231
Mukherjee, Debangshu <mukherjeed@ornl.gov>
2020-07-27 09:41:45 -0400

    added code style badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
 Python based codes for analysis of 4D-STEM and aberration corrected vanilla STEM datasets. 
 Cite as:
 [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3369427.svg)](https://doi.org/10.5281/zenodo.3369427)
--- docs/conf.py
+++ docs/conf.py
@@ -15,88 +15,98 @@ import os
 import mock
 from unittest.mock import MagicMock
 
+
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-            return MagicMock()
+        return MagicMock()
 
-MOCK_MODULES = ['pygtk', 
-                'gtk', 
-                'gobject', 
-                'argparse', 
-                'numpy',
-                'scipy',
-                'pandas', 
-                'skimage', 
-                'pyfftw',
-                'pyfftw.interfaces',
-                'scikit-image',
-                'matplotlib.axes',
-                'numba', 
-                'imagecodecs',
-                'matplotlib',
-                'scipy.optimize',
-                'scipy.ndimage',
-                'matplotlib.pyplot',
-                'matplotlib.colors',
-                'scipy.interpolate', 
-                'skimage.feature', 
-                'pywt', 
-                'pywavelets',
-                'matplotlib.colors', 
-                'matplotlib.gridspec',
-                'matplotlib.cm', 
-                'skimage.color', 
-                'scipy.signal', 
-                'scipy.misc', 
-                'mpl_toolkits.axes', 
-                'mpl_toolkits.axes_grid1', 
-                'scipy.interpolate', 
-                'skimage.restoration', 
-                'scipy.special', 
-                'matplotlib.gridspec',]
+
+MOCK_MODULES = [
+    "pygtk",
+    "gtk",
+    "gobject",
+    "argparse",
+    "numpy",
+    "scipy",
+    "pandas",
+    "skimage",
+    "pyfftw",
+    "pyfftw.interfaces",
+    "scikit-image",
+    "matplotlib.axes",
+    "numba",
+    "imagecodecs",
+    "matplotlib",
+    "scipy.optimize",
+    "scipy.ndimage",
+    "matplotlib.pyplot",
+    "matplotlib.colors",
+    "scipy.interpolate",
+    "skimage.feature",
+    "pywt",
+    "pywavelets",
+    "matplotlib.colors",
+    "matplotlib.gridspec",
+    "matplotlib.cm",
+    "skimage.color",
+    "scipy.signal",
+    "scipy.misc",
+    "mpl_toolkits.axes",
+    "mpl_toolkits.axes_grid1",
+    "scipy.interpolate",
+    "skimage.restoration",
+    "scipy.special",
+    "matplotlib.gridspec",
+]
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
 from unittest.mock import MagicMock
 import matplotlib
-matplotlib.use('agg')
+
+matplotlib.use("agg")
 import sphinx
 import sphinx_rtd_theme
-sys.path.append(os.path.abspath('../'))
+
+sys.path.append(os.path.abspath("../"))
 import stemtool
 
 
 # -- Project information -----------------------------------------------------
 
-project = u'stemtool'
-copyright = u'2020, Debangshu Mukherjee'
-author = u'Debangshu Mukherjee'
-master_doc = 'index'
+project = u"stemtool"
+copyright = u"2020, Debangshu Mukherjee"
+author = u"Debangshu Mukherjee"
+master_doc = "index"
 
 # The full version, including alpha/beta/rc tags
 release = stemtool.__version__
-autoapi_dirs = ['../stemtool']
+autoapi_dirs = ["../stemtool"]
 
-#Add Mock files to fool RTD for C dependent packages
+# Add Mock files to fool RTD for C dependent packages
+
 
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-            return MagicMock()
+        return MagicMock()
 
-MOCK_MODULES = ['pygtk', 
-                'gtk', 
-                'gobject', 
-                'argparse', 
-                'numpy', 
-                'pandas', 
-                'skimage', 
-                'pyfftw', 
-                'scikit-image', 
-                'numba', 
-                'imagecodecs']
+
+MOCK_MODULES = [
+    "pygtk",
+    "gtk",
+    "gobject",
+    "argparse",
+    "numpy",
+    "pandas",
+    "skimage",
+    "pyfftw",
+    "scikit-image",
+    "numba",
+    "imagecodecs",
+]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
@@ -106,19 +116,19 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
-    'recommonmark',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "recommonmark",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # General information about the project.
 
@@ -126,20 +136,21 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 add_function_parentheses = True
 add_module_names = True
 show_authors = True
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 
-autoclass_content = 'both'
+autoclass_content = "both"
 
-autodoc_default_flags = ['members',
-                         'inherited-members',
-                         # 'private-members',
-                         # 'show-inheritance'
-                         ]
+autodoc_default_flags = [
+    "members",
+    "inherited-members",
+    # 'private-members',
+    # 'show-inheritance'
+]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -147,9 +158,9 @@ autodoc_default_flags = ['members',
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
